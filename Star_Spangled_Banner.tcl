f2a175ae23be12e36177f210143c5510cdc99822
Amol-Sone <eon747@live.com>
2017-03-17 01:54:11 -0400

    Updated Star_Spangled_Banner.tcl
    Fixed multiple syntax errors.

--- Star_Spangled_Banner.tcl
+++ Star_Spangled_Banner.tcl
@@ -1,15 +1,15 @@
-puts "Oh, say can you see/n
-By the dawns early light/n
-What so proudly we hailed/n
-At the twilights last gleaming?/n
-Who's broad striped and bright stars/n
-Through the perilous fight/n
-O'er the ramparts we watched/n
-Were so gallantly streaming?/n
-And the rocket's red glare!/n
-The bombs bursting in air!/n
-Gave proof through the night/n
-That our flag was still there/n
-Oh, say does that star spangled banner yet wave/n
-O'er the land of the free/n
+puts "Oh, say can you see\n
+By the dawns early light\n
+What so proudly we hailed\n
+At the twilights last gleaming?\n
+Who's broad striped and bright stars\n
+Through the perilous fight\n
+O'er the ramparts we watched\n
+Were so gallantly streaming?\n
+And the rocket's red glare!\n
+The bombs bursting in air!\n
+Gave proof through the night\n
+That our flag was still there\n
+Oh, say does that star spangled banner yet wave\n
+O'er the land of the free\n
 And the home of the brave?"
